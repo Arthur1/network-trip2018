b9f44158b8bf51767076a68f204b295141265b45
Kazuki Asakura <32762324+arthur-teleneco@users.noreply.github.com>
2018-02-24 15:24:32 +0900

    TT

--- fuel/app/views/guide/schedule.php
+++ fuel/app/views/guide/schedule.php
@@ -14,25 +14,45 @@
 			<dt class="light-green-text text-darken-1">8:40</dt>
 			<dd>自由が丘駅南口改札集合</dd>
 			<dt class="light-green-text text-darken-1">9:00</dt>
-			<dd>トヨタレンタカー自由が丘店</dd>
-			<dt class="light-green-text text-darken-1">9:30</dt>
-			<dd>出発</dd>
-			<dt class="light-green-text text-darken-1">?</dt>
+			<dd>トヨタレンタカー自由が丘店・出発</dd>
+			<dt class="light-green-text text-darken-1">9:40～9:50</dt>
 			<dd>休憩①海老名SA</dd>
-			<dt class="light-green-text text-darken-1">?</dt>
+			<dt class="light-green-text text-darken-1">10:20～10:30</dt>
 			<dd>休憩②足柄SA</dd>
-			<dt class="light-green-text text-darken-1">11:00</dt>
+			<dt class="light-green-text text-darken-1">11:00～13:00</dt>
 			<dd>炭焼きレストランさわやか長泉店</dd>
-			<dt class="light-green-text text-darken-1">?</dt>
-			<dd>MSM大社</dd>
+			<dt class="light-green-text text-darken-1">13:30～14:30</dt>
+			<dd>MSM大社・大社の杜</dd>
 			<dt class="light-green-text text-darken-1">18:00</dt>
-			<dd>グリーンヒル八ヶ岳</dd>
+			<dd>グリーンヒル八ヶ岳到着</dd>
+			<dt class="light-green-text text-darken-1">20:00</dt>
+			<dd>夕食(自炊)</dd>
 		</dl>
 	</div>
 	<div id="day2" class="col s12">
 		<h2 class="pink-text">2日目</h2>
+		<dl>
+			<dt class="light-green-text text-darken-1">7:30～9:00</dt>
+			<dd>朝食バイキング</dd>
+			<dt class="light-green-text text-darken-1">?</dt>
+			<dd>自由行動</dd>
+			<dt class="light-green-text text-darken-1">18:00～20:00</dt>
+			<dd>夕食</dd>
+		</dl>
 	</div>
 	<div id="day3" class="col s12">
 		<h2 class="pink-text">3日目</h2>
+		<dl>
+			<dt class="light-green-text text-darken-1">7:30～8:00</dt>
+			<dd>朝食バイキング</dd>
+			<dt class="light-green-text text-darken-1">8:30</dt>
+			<dd>出発</dd>
+			<dt class="light-green-text text-darken-1">9:30</dt>
+			<dd>桔梗屋本社工場</dd>
+			<dt class="light-green-text text-darken-1">?</dt>
+			<dd>いちご狩り？</dd>
+			<dt class="light-green-text text-darken-1">20:00</dt>
+			<dd>レンタカー返却期限</dd>
+		</dl>
 	</div>
 </div>
--- fuel/app/views/guide/stay.php
+++
@@ -1,2 +0,0 @@
-<h1 class="blue-text">宿泊先</h1>
-<a href="http://www.kenkoumura.jp/" target="_blank">グリーンヒル八ヶ岳公式サイト</a>
